f283b51241e3af7a0f8fdc45984fca3f0e5f81a3
Zaran <arcturus5271@gmail.com>
2025-12-26 14:10:23 +0700

    Update Human.java

--- Human.java
+++ Human.java
@@ -2,5 +2,4 @@ public abstract class Human extends LivingThing {
     public Human(String name) {
         super(name);
     }
-    // Kita biarkan grow dan action tetap abstract karena profesi manusia beda-beda
 }
